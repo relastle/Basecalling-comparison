a83e11f1ccd515142f322a594890e6db594e8354
Hiroki Konishi <ind.med.swim.ut.713k@gmail.com>
2018-10-01 15:21:56 +0900

    [add] chiron call script

---
+++ run_chiron.sh
@@ -0,0 +1,8 @@
+python2 /opt/Chiron/chiron/entry.py \
+    call \
+    -i ./01_raw_fast5 \
+    -o ./02_basecalled_reads/chiron \
+    -m /opt/Chiron/chiron/model/DNA_default \
+    --segment_len 400 \
+    -t 10 \
+    --beam 30
