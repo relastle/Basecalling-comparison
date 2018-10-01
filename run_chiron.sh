6418d959c61129cd10b6c665ca8924f363b9de82
Hiroki Konishi <ind.med.swim.ut.713k@gmail.com>
2018-10-01 17:24:55 +0900

    [update] change chiron out dir

--- run_chiron.sh
+++ run_chiron.sh
@@ -1,7 +1,7 @@
 python2 /opt/Chiron/chiron/entry.py \
     call \
     -i ./01_raw_fast5 \
-    -o ./02_basecalled_reads/chiron \
+    -o ./02_basecalled_reads.tmp/chiron \
     -m /opt/Chiron/chiron/model/DNA_default \
     --segment_len 400 \
     -t 10 \
