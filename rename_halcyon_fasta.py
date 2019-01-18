90229d79bac9a456e72d625627998a2e06048266
Hiroki Konishi <ind.med.swim.ut.713k@gmail.com>
2019-01-18 11:06:25 +0900

    Add: script for renaming halcyon

---
+++ rename_halcyon_fasta.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def main() -> None:
+    with open(args.path, 'r') as f:
+        lines = f.readlines()
+    res_lines = []
+    for i, line in enumerate(lines):
+        if i % 2 == 0:
+            new_line = line.strip().replace('./', '')
+            res_lines.append(new_line)
+        else:
+            res_lines.append(line.strip())
+    for line in res_lines:
+        print(line)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("path", type=str)
+    args = parser.parse_args()
+    main()
